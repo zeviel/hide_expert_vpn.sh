a26a71e7e6e168a6ac35c1397d4282f6d2d8aaa4
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 20:13:40 +0800

    Update hide_expert_vpn.sh

--- src/hide_expert_vpn.sh
+++ src/hide_expert_vpn.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 api="https://api.hide.expert/api"
+user_agent="okhttp/4.9.0"
 
 function get_servers() {
 	curl --request GET \
 		--url "$api/servers" \
-		--user-agent "okhttp/4.9.0" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json" \
 		--header "accesstoken: afkdkfbskodfsighp"
 }
